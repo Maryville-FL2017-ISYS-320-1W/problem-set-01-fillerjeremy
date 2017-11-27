2a4c71f96bc575105d31be791a8f96461167e2f2
fillerjeremy <33054901+fillerjeremy@users.noreply.github.com>
2017-11-26 20:05:22 -0800

    NumberString

--- src/NumberString.java
+++ src/NumberString.java
@@ -5,7 +5,8 @@
 */
 
 /* 6. Your pseudocode algorithm here
-
+One statement prints a line that says 000111222333444555666777888999
+Next statement prints that line 3 times
 
 */
 
@@ -13,7 +14,10 @@ public class NumberString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		for (int i = 1; i <= 3; i++) {
+			System.out.println("000111222333444555666777888999");
+		}
+		
 	}
 
 }
