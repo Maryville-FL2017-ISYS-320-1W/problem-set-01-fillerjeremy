231bf841685a78c6248088c6da07cf2997cc625b
fillerjeremy <33054901+fillerjeremy@users.noreply.github.com>
2017-11-26 19:52:36 -0800

    NestedLoopBox

--- src/NestedLoopBox.java
+++ src/NestedLoopBox.java
@@ -1,18 +1,26 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Jeremy Filler
+  	Date: 11/26/17
 */
 
 /* 4. Your pseudocode algorithm here
-
+First statement writes 5 asterisks in 1 line.
+Second statement does the first statement a variable amount of times.
 
 */
 public class NestedLoopBox {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+			
+	for (int i = 1; i <= 6; i++) {
+	Stars();
+	}
+	}
+	private static void Stars() {
+			System.out.println("*****");
+		
 	}
 
-}
+	}
